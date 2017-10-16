24db4eab22da408b61b2d10f7fd79b05d7d1f6e6
gonglinyuan <gonglinyuan@163.com>
2017-10-16 15:03:31 +0800

    Implemented test
    still WIP

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def normalize(x: np.ndarray, mean: np.ndarray = None, std: np.ndarray = None) ->
 
 def run(x: np.ndarray, y: np.ndarray, *, hidden_units: int, iter_num: int, friction: float, learning_rate: float) -> (
         float, float):
-    train_cost, validation_cost = 0., 0.
+    train_cost, validation_cost, train_acc, validation_acc = 0.0, 0.0, 0.0, 0.0
     x = normalize(x)
     for cv_id in range(SUBSET_NUM):
         idx_train = np.ones(x.shape[1], np.bool)
@@ -74,13 +74,26 @@ def run(x: np.ndarray, y: np.ndarray, *, hidden_units: int, iter_num: int, frict
         x_validate, y_validate = x[:, idx_validate], y[:, idx_validate]
         w, b = nN.init((x_train.shape[0], hidden_units, y_train.shape[0]), distribution="UNIFORM", dev_type="FAN_IN")
         w, b = nN.optimize(w, b, x_train, y_train, iter_num=iter_num, friction=friction, learning_rate=learning_rate)
-        y_train_predicted = nN.forward_propagation(w, b, x_train)[-1]
-        y_validate_predicted = nN.forward_propagation(w, b, x_validate)[-1]
-        train_cost = train_cost + nN.cost(y_train, y_train_predicted)
-        validation_cost = validation_cost + nN.cost(y_validate, y_validate_predicted)
-        # print(np.sum(np.logical_xor(y_train, y_train_predicted >= 0.5)) / y_train.shape[1])
-        # print(np.sum(np.logical_xor(y_validate, y_validate_predicted >= 0.5)) / y_validate.shape[1])
+        y_train_p = nN.forward_propagation(w, b, x_train)[-1]
+        y_validate_p = nN.forward_propagation(w, b, x_validate)[-1]
+        train_cost = train_cost + nN.cost(y_train, y_train_p)
+        validation_cost = validation_cost + nN.cost(y_validate, y_validate_p)
+        train_acc = train_acc + np.sum(np.logical_xor(y_train, y_train_p >= 0.5)) / y_train.shape[1]
+        validation_acc = validation_acc + np.sum(np.logical_xor(y_validate, y_validate_p >= 0.5)) / y_validate.shape[1]
+    train_acc, validation_acc = 1.0 - train_acc / SUBSET_NUM, 1.0 - validation_acc / SUBSET_NUM
+    print(train_acc, validation_acc)
     return train_cost / SUBSET_NUM, validation_cost / SUBSET_NUM
+
+
+def run_test(x_train: np.ndarray, y_train: np.ndarray, x_test, *, hidden_units: int, iter_num: int, friction: float,
+             learning_rate: float) -> np.ndarray:
+    mean = np.mean(np.concatenate((x_train, x_test), axis=1), axis=1, keepdims=True)
+    std = np.std(np.concatenate((x_train, x_test), axis=1), axis=1, keepdims=True)
+    x_train, x_test = normalize(x_train, mean, std), normalize(x_test, mean, std)
+    w, b = nN.init((x_train.shape[0], hidden_units, y_train.shape[0]), distribution="UNIFORM", dev_type="FAN_IN")
+    w, b = nN.optimize(w, b, x_train, y_train, iter_num=iter_num, friction=friction, learning_rate=learning_rate)
+    y_test_p = nN.forward_propagation(w, b, x_test)[-1]
+    return y_test_p >= 0.5
 
 
 def check_output_status(file_name: str, param_list_hash: str) -> int:
@@ -117,11 +130,11 @@ def resume(file_name: str, run_list: tuple, start_pos: int) -> None:
 
 
 def main():
-    data_x, data_y = preprocess_train(pandas.read_csv("train.csv"))
+    x_train, y_train = preprocess_train(pandas.read_csv("train.csv"))
     param_list = ((10, 1000, 0.1, 0.1),)
     param_list_hash = hashlib.sha256(str(param_list).encode('utf-8')).hexdigest()
     output_status = check_output_status("output.csv", param_list_hash)
-    run_list = tuple(map(lambda params: functools.partial(run, x=data_x, y=data_y,
+    run_list = tuple(map(lambda params: functools.partial(run, x=x_train, y=y_train,
                                                           hidden_units=params[0],
                                                           iter_num=params[1],
                                                           friction=params[2],
@@ -133,7 +146,14 @@ def main():
         resume("output.csv", run_list, output_status)
 
 
+def test():
+    x_train, y_train = preprocess_train(pandas.read_csv("train.csv"))
+    x_test = preprocess_test(pandas.read_csv("test.csv"))
+    y_test = run_test(x_train, y_train, x_test, hidden_units=10, iter_num=1000, friction=0.1, learning_rate=0.1)
+    print(y_test)
+
 main()
+test()
 
 # def test_main():
 #     data_train_subsets = preprocess_train(pandas.read_csv("train.csv"))
